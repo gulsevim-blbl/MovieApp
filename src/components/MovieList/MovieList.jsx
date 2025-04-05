a1a62067de33cf9a7e313c5f003fa3700c7b93a6
gulsevim-blbl <gulsevimblbl@gmail.com>
2025-04-05 21:06:37 +0300

    Added pagination to MyList.jsx and MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,35 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getMovieList, getMovieListByGenre } from "../../redux/slices/movieListSlice";
 import MovieCard from "../MovieCard/MovieCard";
+import ReactPaginate from "react-paginate";
+import { GrPrevious, GrNext } from "react-icons/gr";
 import "./MovieList.css";
 import Loading from "../Loading/Loading";
 
-const MovieList = ({selectedGenre}) => {
+const MovieList = ({ selectedGenre }) => {
   const dispatch = useDispatch();
-
   const { movieList, status, error } = useSelector((store) => store.movieList);
+
+  // Pagination state
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 10;
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = movieList.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(movieList.length / itemsPerPage);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % movieList.length;
+    setItemOffset(newOffset);
+  };
 
   useEffect(() => {
     if (!selectedGenre) {
       dispatch(getMovieList());
-    }else {
+    } else {
       dispatch(getMovieListByGenre(selectedGenre.id));
-    }  
+    }
   }, [selectedGenre, dispatch]);
 
   return (
     <div className="Movie-list">
       <h1>{selectedGenre ? selectedGenre.name : "Discover"}</h1>
       <ul>
-      {status === "fulfilled" ? (
-        movieList?.map((movie) => <MovieCard key={movie.id} movie={movie} />)
-      ) : status === "pending" ? (
-        <Loading />
-      ) : error ? (
-        <Error error={error} />
-      ) : null}
-    </ul>
+        {status === "fulfilled" ? (
+          currentItems.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+        ) : status === "pending" ? (
+          <Loading />
+        ) : error ? (
+          <Error error={error} />
+        ) : null}
+      </ul>
+      {status === "fulfilled" && movieList.length > itemsPerPage && (
+        <div className="pagination-component">
+          <ReactPaginate
+            className="pagination"
+            breakLabel="..."
+            nextLabel={<GrNext />}
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={5}
+            pageCount={pageCount}
+            previousLabel={<GrPrevious />}
+            renderOnZeroPageCount={null}
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/MyList/MyList.jsx
+++ src/pages/MyList/MyList.jsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useSelector } from 'react-redux'
 import MovieCard from '../../components/MovieCard/MovieCard'
+import ReactPaginate from 'react-paginate';
+import { GrPrevious, GrNext } from "react-icons/gr";
 import './MyList.css'
 
 const MyList = () => {
   const { favoriteMovies } = useSelector((store) => store.favorites)
 
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 10 ; 
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = favoriteMovies.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(favoriteMovies.length / itemsPerPage);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % favoriteMovies.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  }
+
   return (
     <div className='my-list'>
-      <ul>
-        {
-          favoriteMovies?.map((movie) => (
-              <MovieCard key={movie.id} movie={movie} />
-            ))
-        }
-      </ul>
+        <h1>Your Favorite Movies</h1>
+        <ul>
+            {
+              currentItems && currentItems.map((movie, index) => (
+                  <MovieCard key={movie.id + index} movie={movie} />
+              ))
+            }
+        </ul>
+        <div className='pagination-component'>
+                <ReactPaginate
+                    className='pagination'
+                    breakLabel="..."
+                    nextLabel={<GrNext />}
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={10}
+                    pageCount={pageCount}
+                    previousLabel={<GrPrevious />}
+                    renderOnZeroPageCount={null}
+                />
+            </div>
     </div>
   )
 }
